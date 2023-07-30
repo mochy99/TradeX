1b3114ac2d1a62732af275e743ab252780de9537
mochy99 <mochynguyen99@gmail.com>
2023-07-29 22:48:25 -0700

    updated query code for database

--- checkEmail.php
+++ checkEmail.php
@@ -5,27 +5,68 @@ ini_set('display_errors', 1);
 
 // start new session
 session_start();
+
+require 'config.php';
 ?>
 
 <?php
 //function to validate that the email is registered or not from database
 
-function isUsed () {
+function isUsed() {
     $email = test_input($_POST["email"]);
-    if ($email === "hello@world.com") {
-        // execute code for the registered email
+
+    // Create connection
+    $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare the SQL statement with a placeholder (?)
+    $sql = "SELECT email FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+
+    // Bind the email value to the placeholder
+    $stmt->bind_param("s", $email);
+
+    // Execute the query
+    $stmt->execute();
+
+    // Get the result
+    $result = $stmt->get_result();
+
+    // Check if the email is already registered
+    if ($result->num_rows > 0) {
         $_SESSION["email"] = $email;
         $_SESSION["registeredMsg"] = "*Your email is already registered!";
         header("Location: signup.php");
         exit;
     } else {
-        // execute code for a new email
         $_SESSION["email"] = $email;
         $_SESSION["registeredMsg"] = "";
         header("Location: nextsignup.php");
         exit;
     }
+
+    // Close the statement and connection
+    $stmt->close();
+    $conn->close();
 }
+
+
+    // if ($email === "hello@world.com") {
+    //     // execute code for the registered email
+    //     $_SESSION["email"] = $email;
+    //     $_SESSION["registeredMsg"] = "*Your email is already registered!";
+    //     header("Location: signup.php");
+    //     exit;
+    // } else {
+    //     // execute code for a new email
+    //     $_SESSION["email"] = $email;
+    //     $_SESSION["registeredMsg"] = "";
+    //     header("Location: nextsignup.php");
+    //     exit;
+    // }
 isUsed();
 function test_input($data) {
     $data = trim($data);
--- checkedUser.php
+++ checkedUser.php
@@ -4,41 +4,85 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
+require 'config.php';
 ?>
 <?php
 function validateLogin() {
-    // Code here to retrieve password from the database based on user input
-    $user = $_POST["email"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
     // SQL query and database operations here
-    if ($user !== "hello@gmail.com") {
-        $getPassword = false; // if email does not exist in DB
-    } else {
-        $getPassword = "@Hello123"; // password retrieved from DB    
+    // Create connection
+    $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " .$conn->connect_error);
     }
+
+    // Prerpare and bind
+    $stmt = $conn->prepare("SELECT password FROM users WHERE email=?");
+    $stmt->bind_param("s", $email);
+
+    // Execute the query
+    $stmt->execute();
+
+    // Get the result
+    $result = $stmt->get_result();
     
-    if (!$getPassword) {
-        $_SESSION["errorMsg"] = "Incorrect username or password.";
-        $_SESSION["isAttemptedLogIn"] = true;
-        header("Location: login.php");
-        exit;
-    } else {
-        // Retrieve password from the database
-        // For example purposes, assigning a static password
-        $_SESSION["errorMsg"] = "";
-        $password_DB = "@Hello123";
-        $password_Page = $_POST["password"];
-        
-        if ($password_DB === $password_Page) {
-            header("Location: landingPage.php");
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if ($password === $row["password"]) {
+            header("Location: mainPage.php");
             exit;
         } else {
-            $_SESSION["errorMsg"] = "Incorrect username or password.";
+            $_SESSION["errorMsg"] =   "*Incorrect username or password";
             $_SESSION["isAttemptedLogIn"] = true;
             header("Location: login.php");
             exit;
         }
+    } else {
+        $_SESSION["errorMsg"] =   "*Incorrect username or password";
+        $_SESSION["isAttemptedLogIn"] = true;
+        header("Location: login.php");
+        exit;
     }
+
+    $stmt->close();
+    $conn->close();
 }
+
+
+
+
+    // if ($user !== "hello@gmail.com") {
+    //     $getPassword = false; // if email does not exist in DB
+    // } else {
+    //     $getPassword = "@Hello123"; // password retrieved from DB    
+    // }
+    
+    // if (!$getPassword) {
+    //     $_SESSION["errorMsg"] = "Incorrect username or password.";
+    //     $_SESSION["isAttemptedLogIn"] = true;
+    //     header("Location: login.php");
+    //     exit;
+    // } else {
+    //     // Retrieve password from the database
+    //     // For example purposes, assigning a static password
+    //     $_SESSION["errorMsg"] = "";
+    //     $password_DB = "@Hello123";
+    //     $password_Page = $_POST["password"];
+        
+    //     if ($password_DB === $password_Page) {
+    //         header("Location: landingPage.php");
+    //         exit;
+    //     } else {
+    //         $_SESSION["errorMsg"] = "Incorrect username or password.";
+    //         $_SESSION["isAttemptedLogIn"] = true;
+    //         header("Location: login.php");
+    //         exit;
+    //     }
+    // }
+
 
 validateLogin();
 ?>
