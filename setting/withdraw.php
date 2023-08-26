c3247232d7a17d4e336a322212b113db01c2d3cf
mochy99 <mochynguyen99@gmail.com>
2023-08-26 14:29:53 -0700

    added include selectMoney

--- setting/withdraw.php
+++ setting/withdraw.php
@@ -17,19 +17,10 @@ include_once "../template/head.php";
     <section class="main">      
         <div class="container">
             <h1>Account Information</h1>
-            <?php include_once '../template/currentBalance.php'; ?>
-            
-            <div>Select amount to withdraw</div>
-            <input type="text" name="money" id="money">
-            <div class="error-money neg"></div>
-    
-            <div class="box2">
-               <input type="button" value="100" id="100">
-               <input type="button" value="500" id="500">
-               <input type="button" value="1000" id="1000">
-               <input type="button" value="2000" id="2000">
-               <input type="button" value="5000" id="5000">
-            </div>
+            <?php 
+            include_once '../template/currentBalance.php';
+            include_once '../template/selectMoney.php';
+            ?>
 
             <div class="box">
                 <input type="text"  required="required" name="recipientName" id="recipientName"
