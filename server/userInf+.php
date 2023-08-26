796ccd2efc9a6932c6334e09b056566ac8845b46
mochy99 <mochynguyen99@gmail.com>
2023-08-25 22:24:43 -0700

    added userInf+ and checkRegisteredEmail

---
+++ server/userInf+.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once "template/error.php";
+require 'config.php';
+session_start();
+?>
+
+<?php
+// Create connection
+$email = $_SESSION['email'];
+$conn =  new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " .$conn->connect_error);
+}
+
+// Create prepare and bind
+$stmt = $conn->prepare("SELECT *
+FROM users WHERE email=?");
+$stmt->bind_param("s", $email);
+
+// Execute the query
+$stmt->execute();
+
+// Get the result
+$result = $stmt->get_result();
+    
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();   
+} else {
+    $response ="err";
+};
+$stmt->close();
+$conn->close();
+
+?>
