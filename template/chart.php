5d085efaa7b954c4744fcaa64077c37d539e5a2b
mochy99 <mochynguyen99@gmail.com>
2023-09-20 13:03:13 -0700

    chart template

---
+++ template/chart.php
@@ -0,0 +1,12 @@
+
+    <section class="graphic highcharts-figure">
+        <div class="graph" id ="graph">
+            
+        </div>
+        <div class="selectors">
+            <button class="time clicked" id ="day">D</button>
+            <button class="time unclicked" id ="month">M</button>
+            <button class="time unclicked"id ="year">Y</button>
+            <button class="time unclicked"id ="full">All</button>
+        </div>   
+    </section>
