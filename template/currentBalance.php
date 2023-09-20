c84f415d2c1ee3d10a56ce33f6b437411fa2a366
mochy99 <mochynguyen99@gmail.com>
2023-09-20 13:04:03 -0700

    updated currentBalance template

--- template/currentBalance.php
+++ template/currentBalance.php
@@ -4,3 +4,11 @@
     <span id="currentBalance"><?php echo $user['balance']; ?></span>
     <div class="border-bot"></div>   
 </div>
+<div id="available-sell" class="hidden" >
+    <div class="border-bot"></div>
+    <h2>Available asset</h2>
+    <span class='asset'></span>
+    <h2>Available quantity</h2>
+    <span class="quantity"></span>
+    <div class="border-bot"></div>   
+</div>
