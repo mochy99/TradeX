cb65ff5eae549c0b51335d964070f63fda8d8c81
mochy99 <119394155+mochy99@users.noreply.github.com>
2023-10-05 23:45:54 -0700

    Delete userInteract.js

--- userInteract.js
+++
@@ -1,7 +0,0 @@
-let main = document.querySelector(".main");
-let summary = document.querySelector(".summary");
-
-summary.addEventListener("scroll", (eve) => {
-    console.log(eve);
-    console.log(summary.getBoundingClientRect.top);
-})
