d88ebc1ea9cb0cc7d228e6b9b60e8023ee8938ff
mochy99 <mochynguyen99@gmail.com>
2023-10-09 00:18:34 -0700

    changed html

--- template/currentBalance.php
+++ template/currentBalance.php
@@ -4,11 +4,13 @@
     <span id="currentBalance"><?php echo $user['balance']; ?></span>
     <div class="border-bot"></div>   
 </div>
-<div id="available-sell" class="hidden" >
+<div id="available-sell" class="available hidden" >
     <div class="border-bot"></div>
-    <h2>Available asset</h2>
-    <span class='asset'></span>
-    <h2>Available quantity</h2>
-    <span class="quantity"></span>
+
+    <h2>Available Asset: <span class='asset'></span></h2>
+
+
+    <h2>Available <span class="quantity"></span></h2>
+
     <div class="border-bot"></div>   
 </div>
