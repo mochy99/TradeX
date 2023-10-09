81a55b222589231de1a900301816a489f9d049e0
mochy99 <mochynguyen99@gmail.com>
2023-10-09 00:19:04 -0700

    changed html

--- template/selectMoney.php
+++ template/selectMoney.php
@@ -6,11 +6,11 @@
 
     
     <div class="box2">
-        <input type="button" value="100" id="100">
-        <input type="button" value="500" id="500">
-        <input type="button" value="1000" id="1000">
-        <input type="button" value="2000" id="2000">
-        <input type="button" value="5000" id="5000">
+        <input type="button" class="select-money" value="100" id="100">
+        <input type="button" class="select-money" value="500" id="500">
+        <input type="button" class="select-money" value="1000" id="1000">
+        <input type="button" class="select-money" value="2000" id="2000">
+        <input type="button" class="select-money" value="5000" id="5000">
     </div>
             
 </div>
