9717d09484a5acaa4c273be90f4336f6f9dfacfa
mochy99 <mochynguyen99@gmail.com>
2023-10-09 22:23:48 -0700

    added notes

--- index.php
+++ index.php
@@ -15,11 +15,19 @@ session_destroy();
             <div class="description">Welcome to our cutting-edge trading platform, 
                 where you can unlock endless possibilities in the financial markets.
             </div>
+            <div class="note">
+                This is a simulation application where you can experiment with a fictitious email 
+                and without providing real credit information to understand how the app functions.
+            </div>
+            <div class="note">
+                For trial purpose, you can trial tester@gmail.com with password @Tester123
+            </div>
         </section>
         <section class="btns">
             <button id="signup">Sign up</button>
             <button id="login">Log in</button>
         </section>
+        
     </div>
     
 </body>
